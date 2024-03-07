7fce2f427ec1ec14692a3969d4e9a3758ae2e0c3
Paulo Monterroso <84726204+pmont4@users.noreply.github.com>
2024-03-07 16:08:55 -0600

    Adding beans

--- ws-rest-api/src/main/java/Entity/User.java
+++ ws-rest-api/src/main/java/Entity/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
     private String PASSWORD;
     private String USERNAME;
     private String EMAIL;
-    private Integer ID_ROLE;
+    private Role ROLE;
     private Integer STATUS;
-    
+
 }
