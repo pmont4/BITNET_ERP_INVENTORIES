727fcb756dee13917823100a79bea9a35ea2020f
Paulo Monterroso <84726204+pmont4@users.noreply.github.com>
2024-03-07 14:38:44 -0600

    Some configuration strings and code.

---
+++ ws-rest-api/src/main/java/Entity/User.java
@@ -0,0 +1,26 @@
+package Entity;
+
+import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private Integer ID_USER;
+    private String LOGIN_NAME;
+    private String PASSWORD;
+    private String USERNAME;
+    private String EMAIL;
+    private Integer ID_ROLE;
+    private Integer STATUS;
+    
+}
